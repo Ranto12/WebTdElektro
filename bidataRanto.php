bb9bf2390cee44470778088774a537be2b1e02cd
Ranto <rantokoplak258@gmail.com>
2021-12-01 00:00:39 +0700

    tambahan
    jos kan

---
+++ bidataRanto.php
@@ -0,0 +1,17 @@
+<?php
+$npm = "1915061036";
+$nama = 'Ranto';
+$alamat = "Lampung Selatan";
+$umur = 20;
+$nilai = 99;
+$status = TRUE;
+echo "NPM : " . $npm . "<br>";
+echo "Nama : $nama<br>";
+echo "alamat : $alamat <br>";
+print "Umur : " . $umur; print "<br>";
+printf ("Nilai : %.1f<br>", $nilai);
+if ($status)
+echo "Status : sudah pacaran";
+else
+echo "Status : Tidak pacaran";
+?>
